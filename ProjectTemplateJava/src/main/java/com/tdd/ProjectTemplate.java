a0d79b76b91e677a1ae735a4d86c5947ac1fab36
Ana Caceres <alu0100972016@ull.edu.es>
2019-03-20 10:14:28 +0000

    Creación del template para realizar TDD en Java

---
+++ ProjectTemplateJava/src/main/java/com/tdd/ProjectTemplate.java
@@ -0,0 +1,6 @@
+package com.tdd;
+
+public class ProjectTemplate {
+
+}
+
---
+++ ProjectTemplateJava/src/test/java/com/tdd/ProjectTemplateTest.java
@@ -0,0 +1,12 @@
+package com.tdd;
+
+import org.junit.Test;
+
+public class ProjectTemplateTest {
+
+    @Test
+    public void test() {
+        
+    }
+
+}
